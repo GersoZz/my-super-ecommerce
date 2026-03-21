1a24bcf172ad92c357770a098bcc13f928aaa719
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-03-20 22:18:29 -0500

    chore: implement Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ import ProductCard from './components/ProductCard'
 function App() {
   return (
     <>
-      <ProductCard 
-        title="Cat Toy" 
-        description="Juguete para gato" 
-        price="100" 
-        imageUrl="https://cdn.petsathome.com/public/images/products/900_7111664.jpg" />
-      <ProductCard 
-        title="Dog Toy" 
-        description="Juguete para perro" 
-        price="200" 
+      <ProductCard
+        title="Cat Toy"
+        description="Juguete para gato"
+        price="100"
+        imageUrl="https://cdn.petsathome.com/public/images/products/900_7111664.jpg"
       />
+      <ProductCard title="Dog Toy" description="Juguete para perro" price="200" />
     </>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,30 +3,23 @@ import './ProductCard.css'
 
 // props
 function ProductCard({ title, description, price, imageUrl = 'https://picsum.photos/600/400' }) {
-
-    const [isAdded, setIsAdded] = useState(false)
+  const [isAdded, setIsAdded] = useState(false)
 
   return (
-    <div id='card-01' className='card'>
-        <img src={imageUrl} alt={title}
-        className="card-image"
-        />
+    <div id="card-01" className="card">
+      <img src={imageUrl} alt={title} className="card-image" />
 
-        <div className="card-content">
-            <h3 className="card-title">{title}</h3>
-            <p className="card-text">
-                {description}
-            </p>
+      <div className="card-content">
+        <h3 className="card-title">{title}</h3>
+        <p className="card-text">{description}</p>
 
-            <p className = "card-footer">
-                <span className="card-price">${price}</span>
-                <button 
-                    onClick={()=> setIsAdded(!isAdded)}
-                    className={isAdded ? 'card-button added': 'card-button'}>
-                        {isAdded ? 'Agregado' : 'Agregar'}
-                </button>
-            </p>
-         </div>
+        <p className="card-footer">
+          <span className="card-price">${price}</span>
+          <button onClick={() => setIsAdded(!isAdded)} className={isAdded ? 'card-button added' : 'card-button'}>
+            {isAdded ? 'Agregado' : 'Agregar'}
+          </button>
+        </p>
+      </div>
     </div>
   )
 }
