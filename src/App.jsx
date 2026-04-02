38289c95c8245e02a3d03307e99cb7d879d98c48
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-04-01 21:34:14 -0500

    feat: replace hardcoded product data with API fetch for dynamic product listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import PromoBanner from './components/PromoBanner'
 import Header from './components/Header'
@@ -8,28 +8,15 @@ import Container from './components/Container'
 function App() {
   const [showPromo, setShowPromo] = useState(true)
 
-  const productsData = [
-    {
-      id: '01',
-      title: 'Cat Toy',
-      description: 'Juguete para gato',
-      price: 100,
-      imageUrl: 'https://cdn.petsathome.com/public/images/products/900_7111664.jpg',
-    },
-    {
-      id: '02',
-      title: 'Dog Toy',
-      description: 'Juguete para perro',
-      price: 200,
-    },
-    {
-      id: '03',
-      title: 'Parrot Toy',
-      description: 'Juguete para Loro',
-      price: 200,
-      imageUrl: 'https://i.etsystatic.com/24759185/r/il/99c176/3021454798/il_570xN.3021454798_exyo.jpg',
-    },
-  ]
+  const [productsData, setProductsData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=6')
+      .then((res) => res.json())
+      .then((data) => {
+        setProductsData(data)
+      })
+  }, [])
 
   const onClosePromo = () => {
     setShowPromo(false)
