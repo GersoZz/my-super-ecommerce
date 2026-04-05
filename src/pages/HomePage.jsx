00d428273e2938f2add7b035f2d529b326f181f0
GersoZz <37023982+GersoZz@users.noreply.github.com>
2026-04-04 21:29:42 -0500

    feat: restructure App component to use HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+
+import PromoBanner from '../components/PromoBanner'
+import Header from '../components/Header'
+import ProductList from '../components/ProductList'
+import Container from '../components/Container'
+import { productAdapter } from '../adapters/products.adapter'
+import Loader from '../components/Loader'
+import useFetch from '../hooks/useFetch'
+import ErrorMessage from '../components/ErrorMessage'
+import { API_ENDPOINTS } from '../utils/constant'
+
+function HomePage() {
+  const [showPromo, setShowPromo] = useState(true)
+
+  const {
+    data: productsAvailableRawData,
+    isLoading: isLoadingAvailable,
+    error: errorAvailable,
+  } = useFetch(API_ENDPOINTS.PRODUCTS_AVAILABLE)
+
+  const {
+    data: productsSuggestedRawData,
+    isLoading: isLoadingSuggested,
+    error: errorSuggested,
+  } = useFetch(API_ENDPOINTS.PRODUCTS_SUGGESTED)
+
+  const {
+    data: productsBestSellerRawData,
+    isLoading: isLoadingBestSeller,
+    error: errorBestSeller,
+  } = useFetch(API_ENDPOINTS.PRODUCTS_BEST_SELLER)
+
+  const productsAvailableData = productsAvailableRawData === null ? [] : productsAvailableRawData.map(productAdapter)
+  const productsSuggestedData = productsSuggestedRawData === null ? [] : productsSuggestedRawData.map(productAdapter)
+  const productsBestSellerData = productsBestSellerRawData === null ? [] : productsBestSellerRawData.map(productAdapter)
+
+  const onClosePromo = () => {
+    setShowPromo(false)
+  }
+
+  return (
+    <>
+      <Header />
+      {showPromo && (
+        <PromoBanner
+          onClose={onClosePromo}
+          totalSeconds={30}
+        />
+      )}
+
+      {errorAvailable && <ErrorMessage message={'No se pudo cargar los productos disponibles'} />}
+      {errorSuggested && <ErrorMessage message={'No se pudo cargar los productos sugeridos'} />}
+      {errorBestSeller && <ErrorMessage message={'No se pudo cargar los productos más vendidos'} />}
+
+      {!errorAvailable && (
+        <Container title="Productos disponibles">
+          <p style={{ fontSize: '1.5rem' }}>Explora nuestra selección de productos disponibles</p>
+          {isLoadingAvailable ? <Loader /> : <ProductList productsData={productsAvailableData} />}
+        </Container>
+      )}
+
+      {!errorSuggested && (
+        <Container title="Productos sugeridos">
+          <p style={{ fontSize: '1.5rem' }}>Explora nuestra selección de productos sugerido</p>
+          {isLoadingSuggested ? <Loader /> : <ProductList productsData={productsSuggestedData} />}
+        </Container>
+      )}
+      {!errorBestSeller && (
+        <Container title="Productos más vendidos">
+          <p style={{ fontSize: '1.5rem' }}>Explora nuestra selección de productos más vendidos</p>
+          {isLoadingBestSeller ? <Loader /> : <ProductList productsData={productsBestSellerData} />}
+        </Container>
+      )}
+    </>
+  )
+}
+
+export default HomePage
